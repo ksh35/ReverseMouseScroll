32657890c7718e61c84232a9d7fc62573f8c8a39
ksh35 <krishmeghani01@gmail.com>
2024-05-18 23:02:41 -0700

    Initial Commit

---
+++ ReverseMouseScroll/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ReverseMouseScroll
+//
+//  Created by Krish Meghani on 5/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ReverseMouseScroll/ReverseMouseScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReverseMouseScrollApp.swift
+//  ReverseMouseScroll
+//
+//  Created by Krish Meghani on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct ReverseMouseScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
